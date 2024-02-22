09b57793d5cfda8863f211f6a8d97d7b928b2032
shubhampathak <shubham.pathak926@gmail.com>
2024-02-22 18:50:06 +0530

    package.json updated for production build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,7 +8,7 @@ import connnectToMongoDB from "./config/db.js";
 import cookieparser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
-
+const __dirname = path.resolve();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
@@ -16,14 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 
 // route for home check
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connnectToMongoDB();
